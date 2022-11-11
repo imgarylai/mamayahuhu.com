6b87886a1908515872d2f762356c1d7b916ed9da
Gary Lai <garylai1990@gmail.com>
2022-11-10 22:08:14 -0800

    Revert "feat(nextjs): enable experimental-edge"
    This reverts commit ef579d248c5540377f0a0ccddbbd4b399b63cce7.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge'
-  },
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
 }
 
 module.exports = nextConfig
